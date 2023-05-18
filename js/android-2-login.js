b99ce58c80fd453d86940ff998ae4baeee7285e9
Willian Massalai Tolfo <willianmassalai@gmail.com>
2023-05-17 22:10:07 -0300

    tela de logine cadastro funcional

---
+++ js/android-2-login.js
@@ -0,0 +1,18 @@
+function fazerLogin() {
+    var email = document.getElementById('Email').value
+    var password = document.getElementById('senha').value
+
+    // Obtém os perfis armazenados no localStorage
+    var perfis = JSON.parse(localStorage.getItem('Perfis')) || []
+
+    // Verifica se há um perfil com o email e a senha fornecidos
+    var perfilEncontrado = perfis.find(function (perfil) {
+        return perfil.email === email && perfil.password === password;
+    })
+
+    if (perfilEncontrado) {
+        window.location.href = "android-3-home.html"
+    } else {
+        alert("Email ou senha inválidos")
+    }
+}
--- js/android-2-singup.js
+++ js/android-2-singup.js
@@ -1,3 +1,7 @@
+function salvar() {
+    salvarPerfil()
+}
+
 const photo = document.getElementById('img-perfil')
 const file = document.getElementById('FlImage')
 
@@ -5,3 +9,92 @@ photo.addEventListener('click', () => {
     file.click()
 })
 
+file.addEventListener('change', () => {
+
+    if (file.files.length <= 0) {
+        return
+    }
+
+    const leitor = new FileReader()
+
+    leitor.onload = () => {
+        photo.src = leitor.result
+    }
+
+    leitor.readAsDataURL(file.files[0])
+
+})
+
+function salvarPerfil() {
+    // Obtém o valor do campo de e-mail
+    var emailSave = document.getElementById('Email').value
+    var imageSave = document.getElementById('FlImage').value
+    var nameSave = document.getElementById('Nome').value
+    var occupationSave = document.getElementById('Occupation').value
+    var passwordSave = document.getElementById('Senha').value
+
+    // Expressão regular para verificar o formato do e-mail
+    var verificationEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
+
+    // Verifica se o e-mail está correto
+    if (!verificationEmail.test(emailSave)) {
+        alert("E-mail incorreto")
+        return
+    }
+
+    // Verifica se o e-mail já existe no localStorage
+    var perfisS = JSON.parse(localStorage.getItem('Perfis')) || []
+    for (var i = 0; i < perfisS.length; i++) {
+        if (perfisS[i].email === emailSave) {
+            alert("E-mail já cadastrado")
+            return;
+        }
+    }
+
+    //Verificação da senha
+
+    var verificationPassword = /(?=.*[A-Z].*[A-Z])(?=.*[a-z].*[a-z])(?=.*\d.*\d)/
+    if (!verificationPassword.test(passwordSave)) {
+        alert("Senha não aceita, Digite pelo menos duas letras maiúsculas, duas letras minúsculas e dois números.");
+        return
+    }
+
+    // Verifica se o campo de ocupação está vazio
+
+    if (nameSave.trim() === "") {
+        alert("Adicione um nome")
+        return
+    }
+
+
+    if (occupationSave.trim() === "") {
+        alert("Adicione um ocupação")
+        return
+    }
+
+    // Cria um novo perfil
+    var novoPerfil = {
+        foto: imageSave,
+        usuario: nameSave,
+        occupation: occupationSave,
+        email: emailSave,
+        password: passwordSave,
+
+    };
+
+    // Adiciona o novo perfil aos perfis existentes
+    perfisS.push(novoPerfil)
+
+    // Salva os perfis atualizados no localStorage
+    localStorage.setItem('Perfis', JSON.stringify(perfisS))
+
+    alert("Perfil salvo com sucesso!")
+
+    window.location.href = "android-2-login.html"
+    //Remove os dados dos inputs
+
+    document.getElementById('Email').value = ""
+    document.getElementById('Nome').value = ""
+    document.getElementById('Occupation').value = ""
+    document.getElementById('Senha').value = ""
+}
